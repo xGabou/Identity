19fcc365a4bd0f2740ab0c71bcbff6b87951bb52
xGabou <99047760+xGabou@users.noreply.github.com>
2025-08-15 14:49:08 -0400

    feat: register example command on Forge

--- forge/src/main/java/draylar/identity/IdentityForge.java
+++ forge/src/main/java/draylar/identity/IdentityForge.java
@@ -1,11 +1,20 @@
 package draylar.identity;
 
+import draylar.identity.command.IdentityCommand;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod("identity")
 public class IdentityForge {
     public IdentityForge() {
         Identity.init();
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onRegisterCommands(RegisterCommandsEvent event) {
+        IdentityCommand.register(event.getDispatcher());
     }
 }
-
