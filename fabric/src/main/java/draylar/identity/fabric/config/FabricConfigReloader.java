7d56e72f8adec51d4447246d899e6a96f1882cfe
xGabou <99047760+xGabou@users.noreply.github.com>
2025-08-20 10:36:52 -0400

    Replace Omega config with AutoConfig

--- fabric/src/main/java/draylar/identity/fabric/config/FabricConfigReloader.java
+++ fabric/src/main/java/draylar/identity/fabric/config/FabricConfigReloader.java
@@ -3,16 +3,14 @@ package draylar.identity.fabric.config;
 import draylar.identity.api.platform.ConfigReloader;
 import draylar.identity.api.platform.IdentityPlatform;
 import draylar.identity.fabric.IdentityFabric;
-import draylar.omegaconfig.OmegaConfig;
+import me.shedaniel.autoconfig.AutoConfig;
 
 public class FabricConfigReloader implements ConfigReloader {
     @Override
     public void reloadConfig() {
-        // Use the Forge-compatible config loader (shared class)
-        IdentityFabric.CONFIG = OmegaConfig.register(IdentityFabricConfig.class);
+        AutoConfig.getConfigHolder(IdentityFabricConfig.class).load();
+        IdentityFabric.CONFIG = AutoConfig.getConfigHolder(IdentityFabricConfig.class).getConfig();
         IdentityPlatform.setConfig(IdentityFabric.CONFIG);
-
-
         System.out.println("[Identity] Fabric config reloaded.");
     }
 }
--- fabric/src/main/java/draylar/identity/registry/ModScreensImpl.java
+++ fabric/src/main/java/draylar/identity/registry/ModScreensImpl.java
@@ -1,12 +1,12 @@
 package draylar.identity.registry;
 
 import draylar.identity.fabric.config.IdentityFabricConfig;
-import draylar.omegaconfig.OmegaConfig;
+import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.client.gui.screen.Screen;
 
 public class ModScreensImpl {
     public static Screen getConfigScreen(Screen parent) {
-        return OmegaConfig.getConfigScreen(IdentityFabricConfig.class, parent);
+        return AutoConfig.getConfigScreen(IdentityFabricConfig.class, parent).get();
     }
 }
 
